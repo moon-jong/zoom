b2a63c5a6ba5ba195e343bf6f6f09b2a92b24434
moonjong <dksjfl@naver.com>
2022-09-30 19:45:21 +0900

    delete python file

--- test.py
+++
@@ -1,7 +0,0 @@
-from minio.api import Minio
-client = Minio('218.145.204.234:9000',
-               access_key='huinnoai',
-               secret_key='memo0301!',
-               secure=False)
-
-client.fget_object('sec10', 'sec10_classification.pth', 'sec10_classification.pth', request_headers=None)
