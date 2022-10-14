670a3fb80fec4d18350d6b6edb6739312970c24b
moonjong <dksjfl@naver.com>
2022-10-14 21:00:31 +0900

    add face mesh communication

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,6 +4,12 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const meshCanvas = document.getElementById("myMesh");
+const myCtx = meshCanvas.getContext("2d");
+const meshPeer = document.getElementById("peerMesh")
+const peerCtx = meshPeer.getContext("2d");
+
+
 
 let myStream;
 let muted=false;
@@ -11,16 +17,101 @@ let cameraOff=false;
 let roomName;
 let myPeerConnection;
 let myDataChannel;
+let canvasStream;
+
+canvasStream = meshCanvas.captureStream(30);
+
 
 const call = document.getElementById("call")
 
+const myMesh = new FaceMesh({locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+  }});
+
+const peerMesh = new FaceMesh({locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+  }});
+
+myMesh.setOptions({
+    maxNumFaces: 1,
+    refineLandmarks: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+  });
+myMesh['onResults'](myResults);
+
+peerMesh.setOptions({
+    maxNumFaces: 1,
+    refineLandmarks: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+  });
+
 call.hidden = true;
+myFace.hidden = true;
+
+
+
+function myResults(results) {
+    myCtx.save();
+    myCtx.clearRect(0, 0, meshCanvas.width, meshCanvas.height);
+    // myCtx.drawImage(
+    //     results.image, 0, 0, meshCanvas.width, meshCanvas.height);
+    if (results.multiFaceLandmarks) {
+      for (const landmarks of results.multiFaceLandmarks) {
+        drawConnectors(myCtx, landmarks, FACEMESH_TESSELATION,
+                       {color: '#C0C0C070', lineWidth: 1});
+        drawConnectors(myCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
+        drawConnectors(myCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {color: '#FF3030'});
+        drawConnectors(myCtx, landmarks, FACEMESH_RIGHT_IRIS, {color: '#FF3030'});
+        drawConnectors(myCtx, landmarks, FACEMESH_LEFT_EYE, {color: '#30FF30'});
+        drawConnectors(myCtx, landmarks, FACEMESH_LEFT_EYEBROW, {color: '#30FF30'});
+        drawConnectors(myCtx, landmarks, FACEMESH_LEFT_IRIS, {color: '#30FF30'});
+        drawConnectors(myCtx, landmarks, FACEMESH_FACE_OVAL, {color: '#E0E0E0'});
+        drawConnectors(myCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
+      }
+    }
+    myCtx.restore();
+  }
+
+function peerResults(results){
+    peerCtx.save();
+    peerCtx.clearRect(0, 0, meshCanvas.width, meshCanvas.height);
+    // peerCtx.drawImage(
+    //     results.image, 0, 0, meshCanvas.width, meshCanvas.height);
+    if (results.multiFaceLandmarks) {
+      for (const landmarks of results.multiFaceLandmarks) {
+        drawConnectors(peerCtx, landmarks, FACEMESH_TESSELATION,
+                       {color: '#C0C0C070', lineWidth: 1});
+        drawConnectors(peerCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
+        drawConnectors(peerCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {color: '#FF3030'});
+        drawConnectors(peerCtx, landmarks, FACEMESH_RIGHT_IRIS, {color: '#FF3030'});
+        drawConnectors(peerCtx, landmarks, FACEMESH_LEFT_EYE, {color: '#30FF30'});
+        drawConnectors(peerCtx, landmarks, FACEMESH_LEFT_EYEBROW, {color: '#30FF30'});
+        drawConnectors(peerCtx, landmarks, FACEMESH_LEFT_IRIS, {color: '#30FF30'});
+        drawConnectors(peerCtx, landmarks, FACEMESH_FACE_OVAL, {color: '#E0E0E0'});
+        drawConnectors(peerCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
+      }
+    }
+    peerCtx.restore();
+}
+
+const camera = new Camera(myFace, {
+    onFrame: async () => {
+      await myMesh.send({image: myFace});
+    },
+    width: 1280,
+    height: 1280
+  });
+  
+camera.start();
+
+
 
 async function getCameras(){
     try{
         const devices = await navigator.mediaDevices.enumerateDevices();
         const cameras = devices.filter(device => device.kind === "videoinput")
-        const currentCamera = myStream.getVideoTracks()[0];
         cameras.forEach(camera => {
             const option = document.createElement("option");
             option.value = camera.deviceId;
@@ -30,6 +121,7 @@ async function getCameras(){
             }
             camerasSelect.appendChild(option);
         });
+        const currentCamera = canvasStream.getTracks()[0]
         } catch (e){
         console.log(e);
     }
@@ -92,7 +184,7 @@ async function handleCameraChange(){
     if(myPeerConnection){
         const audioTrack = myStream.getAudioTracks()[0];
         console.log("audio_enabled = ", audioTrack.enabled);
-        const videoTrack = myStream.getVideoTracks()[0];
+        const videoTrack = canvasStream.getVideoTracks()[0];
         const videoSender = myPeerConnection
         .getSenders()
         .find(sender => sender.track.kind === "video");
@@ -127,6 +219,9 @@ async function initCall(){
     makeConnection();
 }
 
+// async function returnCanvas(){
+
+// }
 
 async function handleWelcomeSubmit(event){
     event.preventDefault();
@@ -179,21 +274,52 @@ socket.on("ice", (ice) => {
 });
 
 //RTC Code 
+// function makeConnection(){
+//     myPeerConnection = new RTCPeerConnection();
+//     myPeerConnection.addEventListener("icecandidate", handleIce);
+//     myPeerConnection.addEventListener("addstream", handleAddStream);
+//     myStream
+//         .getTracks()
+//         .forEach(track => myPeerConnection.addTrack(track, myStream));
+// }
 function makeConnection(){
     myPeerConnection = new RTCPeerConnection();
     myPeerConnection.addEventListener("icecandidate", handleIce);
     myPeerConnection.addEventListener("addstream", handleAddStream);
-    myStream
-        .getTracks()
-        .forEach(track => myPeerConnection.addTrack(track, myStream));
+    myPeerConnection.addStream(canvasStream);
+    
+    // myStream
+    //     .getTracks()
+    //     .forEach(track => if(track.){
+
+    //     })
+    // myStream
+    //     .getTracks()
+    //     .forEach(track => myPeerConnection.addTrack(track, canvasStream));
 }
+
 
 function handleIce(data){
     console.log("sent candidtae");
     socket.emit("ice", data.candidate, roomName);
 }
 
+
+// const camera = new Camera(myFace, {
+//     onFrame: async () => {
+//       await faceMesh.send({image: myFace});
+//     },
+//     width: 1280,
+//     height: 720
+//   });
+  
+// camera.start();
+
 function handleAddStream(data) {
     const peerFace = document.getElementById("peerFace");
+    // console.log('this is stream', data.stream);
+
     peerFace.srcObject = data.stream;
+    // peerMesh.send({image: peerFace})
+    
 }
