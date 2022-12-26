208ce246855b2e087b4e7002deb22f2064e97658
moonjong <dksjfl@naver.com>
2022-12-26 22:14:55 +0900

    refactor 5functions

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,7 @@
 import { getHeadInfo, drawLine, fillFaceAll, fillFaceOutline } from "./canvas_utils.js";
 // import {FirstPersonControls} from "./FirstPersonControls.js"
 // import * as THREE from "../three/three.js";
-import { setCameraLight } from "../three/renderModule.js";
+import { setCameraLight, setBonePony, rigFace, removeBodyPony, drawResults } from "../three/renderModule.js";
 
 const socket = io();
 
@@ -10,7 +10,7 @@ const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 const videoElement = document.getElementById("my-video");
 const guideCanvas = document.getElementById("my-guides");
-const live2d = document.getElementById("my-live2d");
+// const live2d = document.getElementById("my-live2d");
 const ul = document.getElementById("videos");
 const alpha = document.getElementById("alpha");
 const beta = document.getElementById("beta");
@@ -42,7 +42,6 @@ const scene = new THREE.Scene();
 scene.visible = false;
 
 const renderCanvas = document.getElementById('threeDID');
-let currentModel, facemesh;
 
 
 let canvasStream = renderCanvas.captureStream(30);
@@ -251,9 +250,6 @@ function handleAddStream(data) {
 /* Kalido app */
 // camera
 const orbitCamera = setCameraLight(30, scene);
-// const fo = 30;
-// const zPosition = 1 / (Math.tan(15 * (Math.PI / 180)));
-// const orbitCamera = new THREE.PerspectiveCamera(fo, 4/3, 0.1, 200);
 
 
 // Main Render Loop
@@ -272,15 +268,16 @@ function animate() {
         currentVrm.scene.children[1].children[0].children[i].visible = false;
     }
 	*/
-	removeBodyPony(currentVrm)
+	  removeBodyPony(currentVrm)
 
 	
   }
   renderer.render(scene, orbitCamera);
 }
-
 animate();
+// animate(currentVrm, clock, renderer, scene, orbitCamera);
 
+// console.log(currentVrm);
 /* VRM CHARACTER SETUP */
 
 // Import Character VRM
@@ -311,71 +308,11 @@ loader.load(
   error => console.error(error),
 );
 
-// Animate Rotation Helper function
-const rigRotation = (
-	name,
-	rotation = { x: 0, y: 0, z: 0 },
-	dampener = 1,
-	lerpAmount = 0.3
-  ) => {
-	if (!currentVrm) {return}
-	const Part = currentVrm.humanoid.getBoneNode(
-	  THREE.VRMSchema.HumanoidBoneName[name]
-	);
-	if (!Part) {return}
-	
-	let euler = new THREE.Euler(
-	  rotation.x * dampener,
-	  rotation.y * dampener,
-	  rotation.z * dampener
-	);
-	let quaternion = new THREE.Quaternion().setFromEuler(euler);
-	Part.quaternion.slerp(quaternion, lerpAmount); // interpolate
-  };
 
+// Animate Rotation Helper function
   
-  let oldLookTarget = new THREE.Euler()
-  const rigFace = (riggedFace) => {
-	  if(!currentVrm){return}
-	  rigRotation("Head", riggedFace.head, 0.7);
-  
-	  // Blendshapes and Preset Name Schema
-	  const Blendshape = currentVrm.blendShapeProxy;
-	  const PresetName = THREE.VRMSchema.BlendShapePresetName;
-	
-	  // Simple example without winking. Interpolate based on old blendshape, then stabilize blink with `Kalidokit` helper function.
-	  // for VRM, 1 is closed, 0 is open.
-	//   riggedFace.eye.l = lerp(clamp(1 - riggedFace.eye.l, 0, 1),Blendshape.getValue(PresetName.Blink), .5)
-	//   riggedFace.eye.r = lerp(clamp(1 - riggedFace.eye.r, 0, 1),Blendshape.getValue(PresetName.Blink), .5)
-	//   riggedFace.eye = Kalidokit.Face.stabilizeBlink(riggedFace.eye,riggedFace.head.y)
-	  riggedFace.eye = Kalidokit.Face.stabilizeBlink({
-		l: lerp(clamp(1 - riggedFace.eye.l, 0, 1), Blendshape.getValue(PresetName.BlinkL), .2), 
-		r: lerp(clamp(1 - riggedFace.eye.r, 0, 1), Blendshape.getValue(PresetName.BlinkR), .2)
-	},riggedFace.head.y)
-	  
-	  Blendshape.setValue(PresetName.BlinkL, riggedFace.eye.l);
-	  Blendshape.setValue(PresetName.BlinkR, riggedFace.eye.r);
-	  
-	  // Interpolate and set mouth blendshapes
-	  Blendshape.setValue(PresetName.I, lerp(riggedFace.mouth.shape.I,Blendshape.getValue(PresetName.I), .25));
-	  Blendshape.setValue(PresetName.A, lerp(riggedFace.mouth.shape.A,Blendshape.getValue(PresetName.A), .25));
-	  Blendshape.setValue(PresetName.E, lerp(riggedFace.mouth.shape.E,Blendshape.getValue(PresetName.E), .25));
-	  Blendshape.setValue(PresetName.O, lerp(riggedFace.mouth.shape.O,Blendshape.getValue(PresetName.O), .25));
-	  Blendshape.setValue(PresetName.U, lerp(riggedFace.mouth.shape.U,Blendshape.getValue(PresetName.U), .25));
-  
-	  //PUPILS
-	  //interpolate pupil and keep a copy of the value
-	  let lookTarget =
-		new THREE.Euler(
-		  lerp(oldLookTarget.x , riggedFace.pupil.y, .4),
-		  lerp(oldLookTarget.y, riggedFace.pupil.x, .4),
-		  0,
-		  "XYZ"
-		)
-	  oldLookTarget.copy(lookTarget);
-	  currentVrm.lookAt.applyer.lookAt(lookTarget);
-  }
-  
+let oldLookTarget = new THREE.Euler();
+
  /* VRM Character Animator */
 const animateVRM = (vrm, results) => {
 	if (!vrm) {
@@ -393,18 +330,18 @@ const animateVRM = (vrm, results) => {
 		runtime:"mediapipe",
 		video:videoElement
 	 });
-	 rigFace(riggedFace);
-
-
-	}
-  };
+	//  rigFace(riggedFace, currentVrm, oldLookTarget);
+  oldLookTarget = rigFace(riggedFace, currentVrm, oldLookTarget);
+  console.log(oldLookTarget);
+  	}
+  }
 
 /* SETUP MEDIAPIPE HOLISTIC INSTANCE */
 /* already setted */
 
 const onResults = (results) => {
 	// Draw landmark guides
-	drawResults(results)
+	drawResults(results, guideCanvas, videoElement);
 	alphaValue.innerHTML = alpha.value * 0.1;
 	betaValue.innerHTML = beta.value * 0.1;
 	gammaValue.innerHTML = gamma.value * 0.1;
@@ -412,11 +349,12 @@ const onResults = (results) => {
 	let headInfo = getHeadInfo(results);
 	if (headInfo !== null){
 
-		console.log(headInfo.position.x, headInfo.position.y, headInfo.faceSize);
+		// console.log(headInfo.position.x, headInfo.position.y, headInfo.faceSize);
 		setBonePony(currentVrm, [(headInfo.position.x - 0.5) * 4/3, -(headInfo.position.y - 0.5), 0, headInfo.faceSize]);
 		// setBackbonePosition(currentVrm, [headInfo.position.x - 0.5, -(headInfo.position.y - 0.5), 0, headInfo.faceSize]);
 	}
 	// Animate model
+  // console.log(currentVrm);
 	animateVRM(currentVrm, results);
   }
 const material = new THREE.LineBasicMaterial({
@@ -438,64 +376,6 @@ holistic.setOptions({
 
 // Pass holistic a callback function
 holistic.onResults(onResults);
-/* set all bones from root to neck position zero */
-
-const removeBodyPony = ( vrm ) => {
-    if ( vrm.scene.children[6].name = "wedel" ){
-        vrm.scene.children[6].visible = false;  
-        vrm.scene.children[5].children[2].visible = false;
-    }
-    else {
-		vrm.scene.children[5].visible = false;  
-		vrm.scene.children[6].children[2].visible = false;
-	}
-    vrm.scene.children[4].visible = false;  // "Kape+Tail"
-    vrm.scene.children[3].visible = false;  // "Kape"
-}
-
-const setBonePony = ( vrm, [x, y, z, size]) =>{
-    vrm.scene.children[0].children[0].position.set(x,y-0.05,0);  // "RootBone"
-    vrm.scene.children[0].children[0].children[0].position.set(0,0,0);  // "HipsBone"
-    vrm.scene.children[0].children[0].children[0].children[4].position.set(0,0,0);  // "SpineBone"
-    vrm.scene.children[0].children[0].children[0].children[4].children[0].position.set(0,0,0);  // "ChestBone"
-    vrm.scene.children[0].children[0].children[0].children[4].children[0].children[1].position.set(x,y,0);// "NeckBone"
-    vrm.scene.children[0].children[0].children[0].children[4].children[0].children[1].children[0].position.set(0,-0.3,0);  // "HeadBone"
-	vrm.scene.children[0].children[0].children[0].children[4].children[0].children[1].children[0].scale.set(size * 10, size * 10, size * 10);
-}
-
-const setBackbonePosition = ( vrm, [x, y, z, size]) =>{
-    vrm.scene.children[0].position.set(x, y, 0);  // "Root"
-    vrm.scene.children[0].children[0].position.set(0,0,0);  // "J_Bip_C_Hips"
-    vrm.scene.children[0].children[0].children[0].position.set(0,0,0);  // "J_Bip_C_Spine"
-    vrm.scene.children[0].children[0].children[0].children[0].position.set(0,0,0);  // "J_Bip_C_Chest"
-    vrm.scene.children[0].children[0].children[0].children[0].children[0].position.set(0,0,0);// "J_Bip_C_UpperChest"
-    vrm.scene.children[0].children[0].children[0].children[0].children[0].children[5].position.set(x,y,0);  // "J_Bip_C_Neck"
-    vrm.scene.children[0].children[0].children[0].children[0].children[0].children[5].children[0].position.set(0,-0.3,0); // "J_Bip_C_Head"
-	vrm.scene.children[0].children[0].children[0].children[0].children[0].children[5].children[0].scale.set(size * 30, size * 30, size * 30);
-}
-
-
-const drawResults = (results) => {
-	guideCanvas.width = videoElement.videoWidth;
-	guideCanvas.height = videoElement.videoHeight;
-	let canvasCtx = guideCanvas.getContext('2d');
-	canvasCtx.save();
-	canvasCtx.clearRect(0, 0, guideCanvas.width, guideCanvas.height);
-	canvasCtx.drawImage(results.image, 0, 0, guideCanvas.width, guideCanvas.height);
-
-	// Use `Mediapipe` drawing functions
-	drawConnectors(canvasCtx, results.multiFaceLandmarks[0], FACEMESH_TESSELATION, {
-	color: "#C0C0C070",
-	lineWidth: 1
-		});
-	if(results.multiFaceLandmarks[0] && results.multiFaceLandmarks[0].length === 478){
-	//draw pupils
-	drawLandmarks(canvasCtx, [results.multiFaceLandmarks[0][468],results.multiFaceLandmarks[0][468+5]], {
-		color: "#ffe603",
-		lineWidth: 2
-		});
-	}
-	}
 
 
 const camera = new Camera(videoElement, {
@@ -514,7 +394,3 @@ guideCanvas.hidden = true;
 guideCanvas.style.webkitTransform = "scaleX(-1)";
 guideCanvas.style.position = 'absolute';
 renderCanvas.style.position = 'absolute';
-
-// camW = myFace.srcObject.getVideoTracks()[0].getSettings().width;
-// camH = myFace.srcObject.getVideoTracks()[0].getSettings().height;
-
--- src/public/three/renderModule.js
+++ src/public/three/renderModule.js
@@ -1,5 +1,7 @@
+const lerp = Kalidokit.Vector.lerp;
+
 function setCameraLight(fov, scene){
-    const fo = 30;
+    const fo = fov;
     const zPosition = 1 / (Math.tan((fo / 2) * (Math.PI / 180)));
     const orbitCamera = new THREE.PerspectiveCamera(fo, 4/3, 0.1, 200);
     
@@ -13,11 +15,13 @@ function setCameraLight(fov, scene){
 }
 
 function animate(vrm, clock, renderer, scene, camera){
-    requestAnimationFrame.apply(animate);
-    if (vrm !== undefined){
+    console.log(vrm);
+    requestAnimationFrame(animate);
+    if (vrm){
         vrm.update(clock.getDelta());
+        removeBodyPony(vrm);
+
     }
-    removeBodyPony(vrm);
     renderer.render(scene, camera);
 }
 
@@ -29,19 +33,127 @@ function rigRotation(
     lerpAmount = 0.3
     
     ){
-    if (vrm === undefined){return}
+    if (!vrm){return}
 
-    const Part = currentVrm.humanoid.getBoneNode(
+    const Part = vrm.humanoid.getBoneNode(
         THREE.VRMSchema.HumanoidBoneName[name]
       );
+    if (!Part) {return}
 
     let euler = new THREE.Euler(
         rotation.x * dampener,
         rotation.y * dampener,
         rotation.z * dampener
       );
-      let quaternion = new THREE.Quaternion().setFromEuler(euler);
+    let quaternion = new THREE.Quaternion().setFromEuler(euler);
 	Part.quaternion.slerp(quaternion, lerpAmount); // interpolate
 }
 
-export{setCameraLight}
+function removeBodyPony(vrm){
+    if ( vrm.scene.children[6].name = "wedel" ){
+        vrm.scene.children[6].visible = false;  
+        vrm.scene.children[5].children[2].visible = false;
+    }
+    else {
+		vrm.scene.children[5].visible = false;  
+		vrm.scene.children[6].children[2].visible = false;
+	}
+    vrm.scene.children[4].visible = false;  // "Kape+Tail"
+    vrm.scene.children[3].visible = false;  // "Kape"
+}
+
+function setBonePony(vrm, [x, y, z, size]){
+    vrm.scene.children[0].children[0].position.set(x,y-0.05,0);  // "RootBone"
+    vrm.scene.children[0].children[0].children[0].position.set(0,0,0);  // "HipsBone"
+    vrm.scene.children[0].children[0].children[0].children[4].position.set(0,0,0);  // "SpineBone"
+    vrm.scene.children[0].children[0].children[0].children[4].children[0].position.set(0,0,0);  // "ChestBone"
+    vrm.scene.children[0].children[0].children[0].children[4].children[0].children[1].position.set(x,y,0);// "NeckBone"
+    vrm.scene.children[0].children[0].children[0].children[4].children[0].children[1].children[0].position.set(0,-0.3,0);  // "HeadBone"
+	vrm.scene.children[0].children[0].children[0].children[4].children[0].children[1].children[0].scale.set(size * 10, size * 10, size * 10);
+}
+
+function setBackbonePosition( vrm, [x, y, z, size]){
+    vrm.scene.children[0].position.set(x, y, 0);  // "Root"
+    vrm.scene.children[0].children[0].position.set(0,0,0);  // "J_Bip_C_Hips"
+    vrm.scene.children[0].children[0].children[0].position.set(0,0,0);  // "J_Bip_C_Spine"
+    vrm.scene.children[0].children[0].children[0].children[0].position.set(0,0,0);  // "J_Bip_C_Chest"
+    vrm.scene.children[0].children[0].children[0].children[0].children[0].position.set(0,0,0);// "J_Bip_C_UpperChest"
+    vrm.scene.children[0].children[0].children[0].children[0].children[0].children[5].position.set(x,y,0);  // "J_Bip_C_Neck"
+    vrm.scene.children[0].children[0].children[0].children[0].children[0].children[5].children[0].position.set(0,-0.3,0); // "J_Bip_C_Head"
+	vrm.scene.children[0].children[0].children[0].children[0].children[0].children[5].children[0].scale.set(size * 30, size * 30, size * 30);
+}
+
+
+const drawResults = (results, guideCanvas, videoElement) => {
+	guideCanvas.width = videoElement.videoWidth;
+	guideCanvas.height = videoElement.videoHeight;
+	let canvasCtx = guideCanvas.getContext('2d');
+	canvasCtx.save();
+	canvasCtx.clearRect(0, 0, guideCanvas.width, guideCanvas.height);
+	canvasCtx.drawImage(results.image, 0, 0, guideCanvas.width, guideCanvas.height);
+
+	// Use `Mediapipe` drawing functions
+	drawConnectors(canvasCtx, results.multiFaceLandmarks[0], FACEMESH_TESSELATION, {
+	color: "#C0C0C070",
+	lineWidth: 1
+		});
+	if(results.multiFaceLandmarks[0] && results.multiFaceLandmarks[0].length === 478){
+	//draw pupils
+	drawLandmarks(canvasCtx, [results.multiFaceLandmarks[0][468],results.multiFaceLandmarks[0][468+5]], {
+		color: "#ffe603",
+		lineWidth: 2
+		});
+	}
+}
+
+function rigFace(riggedFace, vrm, oldLookTarget){
+    if(!vrm){return}
+	  rigRotation(vrm, "Head", riggedFace.head, 0.7);
+  
+	  // Blendshapes and Preset Name Schema
+	  const Blendshape = vrm.blendShapeProxy;
+	  const PresetName = THREE.VRMSchema.BlendShapePresetName;
+	
+	  // Simple example without winking. Interpolate based on old blendshape, then stabilize blink with `Kalidokit` helper function.
+	  // for VRM, 1 is closed, 0 is open.
+	//   riggedFace.eye.l = lerp(clamp(1 - riggedFace.eye.l, 0, 1),Blendshape.getValue(PresetName.Blink), .5)
+	//   riggedFace.eye.r = lerp(clamp(1 - riggedFace.eye.r, 0, 1),Blendshape.getValue(PresetName.Blink), .5)
+	//   riggedFace.eye = Kalidokit.Face.stabilizeBlink(riggedFace.eye,riggedFace.head.y)
+	  riggedFace.eye = Kalidokit.Face.stabilizeBlink({
+		l: lerp(clamp(1 - riggedFace.eye.l, 0, 1), Blendshape.getValue(PresetName.BlinkL), .2), 
+		r: lerp(clamp(1 - riggedFace.eye.r, 0, 1), Blendshape.getValue(PresetName.BlinkR), .2)
+	},riggedFace.head.y)
+	  
+	  Blendshape.setValue(PresetName.BlinkL, riggedFace.eye.l);
+	  Blendshape.setValue(PresetName.BlinkR, riggedFace.eye.r);
+	  
+	  // Interpolate and set mouth blendshapes
+	  Blendshape.setValue(PresetName.I, lerp(riggedFace.mouth.shape.I,Blendshape.getValue(PresetName.I), .25));
+	  Blendshape.setValue(PresetName.A, lerp(riggedFace.mouth.shape.A,Blendshape.getValue(PresetName.A), .25));
+	  Blendshape.setValue(PresetName.E, lerp(riggedFace.mouth.shape.E,Blendshape.getValue(PresetName.E), .25));
+	  Blendshape.setValue(PresetName.O, lerp(riggedFace.mouth.shape.O,Blendshape.getValue(PresetName.O), .25));
+	  Blendshape.setValue(PresetName.U, lerp(riggedFace.mouth.shape.U,Blendshape.getValue(PresetName.U), .25));
+  
+	  //PUPILS
+	  //interpolate pupil and keep a copy of the value
+	  let lookTarget =
+		new THREE.Euler(
+		  lerp(oldLookTarget.x , riggedFace.pupil.y, .4),
+		  lerp(oldLookTarget.y, riggedFace.pupil.x, .4),
+		  0,
+		  "XYZ"
+		)
+	  oldLookTarget.copy(lookTarget);
+	  vrm.lookAt.applyer.lookAt(lookTarget);
+    return oldLookTarget
+}
+
+export{
+    setCameraLight, 
+    animate, 
+    rigRotation, 
+    setBonePony, 
+    removeBodyPony,
+    drawResults,
+    rigFace
+}
