d8d673d2b7848506dbd7865342d516008ec7732a
moonjong <dksjfl@naver.com>
2023-01-01 21:39:05 +0900

    import necessary method

--- src/public/three/renderModule.js
+++ src/public/three/renderModule.js
@@ -1,4 +1,7 @@
 const lerp = Kalidokit.Vector.lerp;
+const modelPath = '/public/vrm/Saitama.vrm'
+const girlModelPath = "https://cdn.glitch.com/29e07830-2317-4b15-a044-135e73c7f840%2FAshtra.vrm?v=1630342336981"
+
 
 function setCameraLight(fov, scene){
     const fo = fov;
